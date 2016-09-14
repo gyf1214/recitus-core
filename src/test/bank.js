0e279463f012dadfad906ad8bc29eebea573d22e
gyf1214 <gyf1214@gmail.com>
2016-09-14 09:35:42 +0800

    add test

---
+++ src/test/bank.js
@@ -0,0 +1,44 @@
+import {unlink} from 'fs';
+import Bank from '../lib/bank';
+
+const vocabPath = 'tmp/hello.json';
+const bankPath = 'tmp/bank.json';
+
+describe('Bank', () => {
+  var bank;
+
+  beforeEach(() => {
+    bank = new Bank(vocabPath, bankPath);
+  });
+
+  it('persists', () => {
+    bank.should.have.properties('bank', 'vocab');
+    var cnt = bank.vocab.length;
+    bank.bank.should.have.lengthOf(cnt);
+    bank.bank[0].expire = Date.today();
+    bank.update();
+    bank = new Bank(vocabPath, bankPath);
+    bank.should.have.property('bank').with.lengthOf(cnt);
+    bank.bank[0].should.have.property('expire', Date.today());
+  });
+
+  it('picks a wordlist', () => {
+    const cnt = 2;
+    var list = bank.pickList(cnt);
+    list.should.have.lengthOf(cnt);
+  });
+
+  it('picks all expired words and filters not expired', () => {
+    const cnt = 2;
+    bank.bank[0].expire = Date.today();
+    bank.bank[1].expire = Date.tomorrow();
+    var list = bank.pickList(cnt);
+    list.should.have.lengthOf(cnt + 1);
+    list.should.containEql(bank.bank[0]);
+    list.should.not.containEql(bank.bank[1]);
+  });
+
+  afterEach((done) => {
+    unlink(bankPath, () => done());
+  });
+});
---
+++ src/test/helpers/helper.js
@@ -0,0 +1,3 @@
+import 'source-map-support/register';
+import 'should';
+export var debug = require('debug')('recitus:test');
